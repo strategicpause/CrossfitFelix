c0f13dee29b81f7a3e151e2399bb5053b20c7867
Nick Peters <nkpeters@amazon.com>
2018-06-13 14:08:04 -0700

    Uncommitted data.

--- data.js
+++ data.js
@@ -1,37 +1,37 @@
 module.exports = {
 	workout: {
-		'4-16-2017': {
-			readoutText: '%t\'s WOD is coaches choice.'
-		},
-		'4-17-2017': {
-			readoutText: '%ts WOD, Man Up, will consist of 3 rounds of Dumbbell man-makers, Toes to bar, and rowing or biking for calories. In the first round you will perform 21 reps of each movement, followed by 15 reps and finally 9 reps on the last round.',
-			title: 'Man Up',
-			displayText: '21-15-9\nDB Man-makers (Renegade Row + Power Clean + S2OH) 35/25 FB: 40/30\nToes-2-Bar\nCals Row/ABike'
-		},
-		'4-18-2017': {
-			readoutText: '%ts WOD, Techies, will consist of 15 burpees to a 6 inch target, followed by 15 squat cleans, and a 400 meter run. Repeat the first two movements 12 times followed by a 400 meter run. For the final round perform the movements 9 times, followed by a 400 meter run.',
-			title: 'Techies',
-			displayText: '15 - 12 - 9\nBurpee FB Burpee Pull-Up\nSquat Cleans 115/85 FB 155/105\n400m Run'
-		},
-		'4-19-2017': {
-			readoutText: '%ts WOD, Turn Up the Volume, is a 17 minute AMRAP. The workout consists of Games style box jumps, alternating single arm dumbell snatch, followed by knees to elbows. For the first round perform each movement 21 times. The next round you will perform each movement 18 times, and continue to decrease by 3 for the following rounds.',
-			title: 'Turn the Volume Up',
-			displayText: '17 Min AMRAP\n21-18-15-12-9-6-3\nGames Style Box Jumps 24/20”\nAlternating Single Arm DB Snatch 35+/25+ FB: Alternating DB Split Snatch 50/40\nKnees-2-Elbow'
-		},
-		'4-20-2017': {
-			readoutText: '',
-			title: 'We Interrupt Your Regular Programming...',
-			displayText: '30/20/10\nE2MOM: 10 Lunges (starting at 0:00 and every 2 mins)\nThrusters 95/65\nRing Rows FB: 3-2-1 Rope Climbs FB+: 1-1-1 Legless Rope Climbs\nBurpees'
-		},
-		'4-21-2017': {
-			readoutText: '%ts WOD, Nasty Girls, consists of 3 rounds of the following for time: 50 Air Squats, 7 Muscle-Ups, and 10 hang power cleans. After the WOD you can choose to do one of the following for time: a 1000m row, 2 miles on a bike, or a 500 meter ski.',
-			title: 'Nasty Girls',
-			displayText: '3 Rounds:\n50 Air Squats\n7 Muscle-ups (scale to 10 Ring Dips)\n10 Hang Power Cleans 135/95\nPost-WOD: time trial (choose one)\n1000m Row\n2 miles on ABike\n500m Ski'
-		},
-		'4-22-2017': {
-			readoutText: '%t\'s WOD is coaches choice.'
-		},
 		'4-23-2017': {
+			readoutText: '%t\'s WOD is coaches choice.'
+		},
+		'4-24-2017': {
+			readoutText: '%ts WOD, Running Game, consists of an 800 meter run, followed by 30 Pushups and 30 Kettle Bell Snatches. Repeat with a 400 meter run, 20 push ups and 20 kettle bell snatches. On the last round perform a 200 meter run, followed by 10 push ups, and 10 kettle bell snatches.',
+			title: 'Running Game',
+			displayText: '800m Run\n30 Push-ups FB: 20 HSPU\n30 KB Snatches 53/35\n400m Run\n20 Push-ups FB: 15 HSPU\n20 KB Snatches\n200m Run\n10 Push-ups FB: 10 HSPU\n10 KB Snatches'
+		},
+		'4-25-2017': {
+			readoutText: '%ts WOD, Power Driven, consists of 3 movements: 15 toes to bar, 10 burpees over the bar, and 5 power clean. Perform five rounds of each movement, each round decreasing the number of toes to bar by 3, the number of burpees by 2 and the number of power cleans by 1.',
+			title: 'Power Driven',
+			displayText: '15 - 12 - 9 - 6 - 3\nToes-2-Bar\n10 - 8 - 6 - 4 - 2\nBurpee Over the Barbell\n5 - 4 - 3 - 2 - 1\nPower Cleans 125/85'
+		},
+		'4-26-2017': {
+			readoutText: '%ts WOD, out of the comfort zone, consists of 4 rounds of dumbell thrusters, full snatches, and box jumps. In the first round you will perform 27 reps of each movement, followed by 21, 15, and finally 9.',
+			title: 'Out of the Comfort Zone',
+			displayText: '27-21-15-9\nDB Thrusters 35/25 FB: 40/30 FB+: 1 Arm\nAlternating DB Full Snatch (choose appropriate weight)\nBox Jumps 24/20'
+		},
+		'4-27-2017': {
+			readoutText: '%ts WOD, Intervals, you will perform the following  movements every minutes for four sets: 400 meter run, 12 burpees and 6 power snatches.',
+			title: 'Intervals',
+			displayText: 'Every 4 mins for 4 sets:\n400m Run\n12 Burpees\n6 Power Snatch 115/75 FB: 135/95\n(rest remaining time...)'
+		},
+		'4-28-2017': {
+			readoutText: '%ts WOD, Blend of Capacities, is a 12 minute AMRAP of the following movements: 8 pullups, 16 kettlebell swings, 24 wallballs, and finally 32 double unders.',
+			title: 'Blend of Capacities',
+			displayText: '12 Min AMRAP\n8 Pullups FB: 4 Bar MUs\n16 KB Swings 53/35 FB: 70/53\n24 Wall Balls 20/14 FB: 30/20\n32 Double Unders'
+		},
+		'4-29-2017': {
+			readoutText: '%t\'s WOD is coaches choice.'
+		},
+		'4-30-2017': {
 			readoutText: '%t\'s WOD is coaches choice.'
 		}
 	},
